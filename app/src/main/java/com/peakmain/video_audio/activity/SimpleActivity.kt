257c56608af233d4b7529e9220a569d083cc6d37
peakmain <2726449200@qq.com>
2021-08-22 18:10:58 +0800

    MediaCodec实现视频播放

--- app/src/main/java/com/peakmain/video_audio/activity/SimpleActivity.kt
+++ app/src/main/java/com/peakmain/video_audio/activity/SimpleActivity.kt
@@ -1,35 +1,37 @@
-package com.peakmain.video_audio
+package com.peakmain.video_audio.activity
 
 import android.content.Intent
-import android.util.Log
 import com.peakmain.ui.recyclerview.listener.OnItemClickListener
-import com.peakmain.video_audio.activity.SimpleActivity
+import com.peakmain.video_audio.R
 import com.peakmain.video_audio.basic.BaseActivity
 import com.peakmain.video_audio.basic.BaseRecyclerStringAdapter
-import com.peakmain.video_audio.utils.crash.NativeCrashMonitor
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity() {
-
+/**
+ * author ：Peakmain
+ * createTime：2021/8/22
+ * mail:2726449200@qq.com
+ * describe：
+ */
+class SimpleActivity:BaseActivity(){
     private var mData: ArrayList<String> = ArrayList()
     override fun getLayoutId(): Int {
         return R.layout.activity_main
     }
 
     override fun initView() {
-        mNavigationBuilder!!.setTitleText("首页").create()
-
+        mNavigationBuilder!!.setTitleText("音视频简单使用").create()
     }
 
     override fun initData() {
-        mData.add("simple的使用")
+        mData.add("MediaCodec实现H264音视频播放")
         val adapter = BaseRecyclerStringAdapter(this, data = mData)
         recycler_view.adapter = adapter
         adapter.setOnItemClickListener(object : OnItemClickListener {
             override fun onItemClick(position: Int) {
                 when (position){
                     0->{
-                    startActivity(Intent(this@MainActivity,SimpleActivity::class.java))
+                        startActivity(Intent(this@SimpleActivity,SimpleActivity::class.java))
                     }
                 }
             }
---
+++ app/src/main/java/com/peakmain/video_audio/simple/H264Player.java
@@ -0,0 +1,11 @@
+package com.peakmain.video_audio.simple;
+
+/**
+ * author ：Peakmain
+ * createTime：2021/8/22
+ * mail:2726449200@qq.com
+ * describe：
+ */
+public class H264Player {
+
+}
